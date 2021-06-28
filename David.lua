a3bbdad747c9e08f73937753c51ba25ff63c5d71
David-Team-Rio <86580440+David-Team-Rio@users.noreply.github.com>
2021-06-28 13:36:11 +0300

    Update David.lua

--- David.lua
+++ David.lua
@@ -10169,7 +10169,7 @@ end
 send(msg.chat_id_, msg.id_,Files)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/DavidTeam8/DavidFiles/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/David-Team-Rio/DavidFiles/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -10201,7 +10201,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/DavidTeam8/DavidFiles/master/DavidFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/David-Team-Rio/DavidFiles/master/DavidFiles/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n↯︙الملف ↫ *"..FileName.."*\n↯︙تم تعطيله وحذفه من البوت بنجاح") 
@@ -10213,7 +10213,7 @@ end
 if text and text:match("^(تفعيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/DavidTeam8/DavidFiles/master/DavidFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/David-Team-Rio/DavidFiles/master/DavidFiles/"..FileName)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..FileName,'w+')
 ChekAuto:write(GetJson)
