2e5e8de5c9fa6b9bd68003d83ae5bca207156e1b
David-Team-Rio <86580440+David-Team-Rio@users.noreply.github.com>
2021-07-10 21:24:05 +0300

    Update David.lua

--- David.lua
+++ David.lua
@@ -3833,8 +3833,7 @@ Welcome To Source
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '↯ Source Channel',url="https://t.me/L9L9L"}},
-{{text = '↯ Developer',url="t.me/tsttt"}},
-{{text = '↯ Tws David',url="https://t.me/W555Bot"}},
+{{text = '↯ Developer',url="t.me/tsttt"},{text = '↯ Tws David',url="https://t.me/W555Bot"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/L9L9L&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
